933cc9e51498da27360567a3f2d4a799b88fdacb
Dan Walker <dwalker109@gmail.com>
2021-12-01 08:48:20 +0000

    Day 1: Sonar Sweep (1/1)

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,22 @@
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Part 1: {}", part_1(include_str!("../input")));
+}
+
+fn part_1(input: &'static str) -> usize {
+    input
+        .lines()
+        .map(|l| l.parse::<usize>().unwrap())
+        .tuple_windows()
+        .fold(0_usize, |acc, (l, r)| acc + (r > l) as usize)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part_1() {
+        let r = super::part_1(include_str!("../input_test"));
+        assert_eq!(r, 7);
+    }
 }
