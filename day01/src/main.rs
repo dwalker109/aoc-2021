2ad6e78af562c48b6e050089f10cba161337e29f
Dan Walker <dwalker109@gmail.com>
2021-12-01 08:57:10 +0000

    Day 1: Sonar Sweep (2/2)

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,21 +2,38 @@ use itertools::Itertools;
 
 fn main() {
     println!("Part 1: {}", part_1(include_str!("../input")));
+    println!("Part 2: {}", part_2(include_str!("../input")));
 }
 
-fn part_1(input: &'static str) -> usize {
+fn part_1(input: &'static str) -> u32 {
     input
         .lines()
-        .map(|l| l.parse::<usize>().unwrap())
+        .map(|l| l.parse::<u32>().unwrap())
         .tuple_windows()
-        .fold(0_usize, |acc, (l, r)| acc + (r > l) as usize)
+        .fold(0, |acc, (l, r)| acc + (r > l) as u32)
+}
+
+fn part_2(input: &'static str) -> u32 {
+    input
+        .lines()
+        .map(|l| l.parse::<u32>().unwrap())
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .fold(0, |acc, (l, r)| acc + (r > l) as u32)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn part_1() {
-        let r = super::part_1(include_str!("../input_test"));
-        assert_eq!(r, 7);
+        let r = super::part_1(include_str!("../input"));
+        assert_eq!(r, 1390);
+    }
+
+    #[test]
+    fn part_2() {
+        let r = super::part_2(include_str!("../input"));
+        assert_eq!(r, 1457);
     }
 }
