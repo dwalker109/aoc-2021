50848dd45aaecb1b378b12b72782c67c8ff320c3
Dan Walker <dwalker109@gmail.com>
2021-12-02 09:18:56 +0000

    Day 2: Dive! (2/2)

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,25 +2,55 @@ use aoc_support::input::DAY_02 as INPUT;
 
 fn main() {
     println!("Part 1: {}", part_1(&INPUT));
+    println!("Part 2: {}", part_2(&INPUT));
 }
 
 fn part_1(input: &'static str) -> u32 {
-    let (hp, d) = input
+    let (h, d) = input
         .lines()
-        .map(|l| {
-            let mut x = l.split_ascii_whitespace();
-            (x.next().unwrap(), x.next().unwrap().parse::<u32>().unwrap())
-        })
-        .fold((0, 0), |acc, (instr, n)| match instr {
-            "forward" => (acc.0 + n, acc.1),
-            "down" => (acc.0, acc.1 + n),
-            "up" => (acc.0, acc.1 - n),
-            _ => panic!(),
+        .map(Instruction::from)
+        .fold((0, 0), |(h, d), instruction| match instruction {
+            Instruction::Forward(n) => (h + n, d),
+            Instruction::Down(n) => (h, d + n),
+            Instruction::Up(n) => (h, d - n),
         });
 
-    hp * d
+    h * d
 }
 
+fn part_2(input: &'static str) -> u32 {
+    let (h, d, _) =
+        input
+            .lines()
+            .map(Instruction::from)
+            .fold((0, 0, 0), |(h, d, a), instruction| match instruction {
+                Instruction::Forward(n) => (h + n, d + a * n, a),
+                Instruction::Down(n) => (h, d, a + n),
+                Instruction::Up(n) => (h, d, a - n),
+            });
+
+    h * d
+}
+
+enum Instruction {
+    Forward(u32),
+    Down(u32),
+    Up(u32),
+}
+
+impl From<&str> for Instruction {
+    fn from(line: &str) -> Self {
+        let [text, num] =
+            <[&str; 2]>::try_from(line.split_ascii_whitespace().collect::<Vec<_>>()).unwrap();
+
+        match text {
+            "forward" => Self::Forward(num.parse::<u32>().unwrap()),
+            "down" => Self::Down(num.parse::<u32>().unwrap()),
+            "up" => Self::Up(num.parse::<u32>().unwrap()),
+            _ => panic!(),
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     static INPUT: &str = include_str!("../input_test");
@@ -29,5 +59,10 @@ mod tests {
     fn part_1() {
         let r = super::part_1(INPUT);
         assert_eq!(r, 150);
+    }
+    #[test]
+    fn part_2() {
+        let r = super::part_2(INPUT);
+        assert_eq!(r, 900);
     }
 }
