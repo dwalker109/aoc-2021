a08f3c3caf9e26fdcc47a62c709eab2125a69ee1
Dan Walker <dwalker109@gmail.com>
2021-12-02 02:05:18 +0000

    Update day 01 to use private downloaded input files

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,9 @@
+use aoc_support::input::DAY_01 as INPUT;
 use itertools::Itertools;
 
 fn main() {
-    println!("Part 1: {}", part_1(include_str!("../input")));
-    println!("Part 2: {}", part_2(include_str!("../input")));
+    println!("Part 1: {}", part_1(&INPUT));
+    println!("Part 2: {}", part_2(&INPUT));
 }
 
 fn part_1(input: &'static str) -> u32 {
@@ -25,15 +26,17 @@ fn part_2(input: &'static str) -> u32 {
 
 #[cfg(test)]
 mod tests {
+    static INPUT: &str = include_str!("../input_test");
+
     #[test]
     fn part_1() {
-        let r = super::part_1(include_str!("../input"));
-        assert_eq!(r, 1390);
+        let r = super::part_1(INPUT);
+        assert_eq!(r, 7);
     }
 
     #[test]
     fn part_2() {
-        let r = super::part_2(include_str!("../input"));
-        assert_eq!(r, 1457);
+        let r = super::part_2(INPUT);
+        assert_eq!(r, 5);
     }
 }
