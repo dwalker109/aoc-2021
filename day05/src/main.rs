2d37ae26b12f4de123a65033303052c0a7fd0145
Dan Walker <dwalker109@gmail.com>
2021-12-05 14:33:10 +0000

    Day 5: Hydrothermal Venture (2/2)

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -7,23 +7,17 @@ static INPUT: &str = include_str!("../input");
 
 fn main() {
     println!("Part 1: {}", part_1(INPUT));
+    println!("Part 2: {}", part_2(INPUT));
 }
 
 fn part_1(input: &'static str) -> u32 {
-    let exp = regex::Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
-    let coordinates = exp.captures_iter(input).map(|caps| {
-        let c = caps
-            .iter()
-            .skip(1)
-            .map(|c| c.unwrap().as_str().parse::<u32>().unwrap())
-            .collect::<Vec<_>>();
-
-        ((c[0], c[1]), (c[2], c[3]))
-    });
-
+    let coordinates = parse_input(input);
     let mut vents = HashMap::new();
 
-    for ((x1, y1), (x2, y2)) in coordinates.filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2) {
+    for ((x1, y1), (x2, y2)) in coordinates
+        .into_iter()
+        .filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2)
+    {
         for x in min(x1, x2)..=max(x1, x2) {
             for y in min(y1, y2)..=max(y1, y2) {
                 *vents.entry((x, y)).or_insert(0) += 1;
@@ -32,6 +26,56 @@ fn part_1(input: &'static str) -> u32 {
     }
 
     vents.values().filter(|&v| *v > 1).count() as u32
+}
+fn part_2(input: &'static str) -> u32 {
+    let coordinates = parse_input(input);
+    let mut vents = HashMap::new();
+
+    for ((x1, y1), (x2, y2)) in coordinates {
+        match x1 == x2 || y1 == y2 {
+            true => {
+                for x in min(x1, x2)..=max(x1, x2) {
+                    for y in min(y1, y2)..=max(y1, y2) {
+                        *vents.entry((x, y)).or_insert(0) += 1;
+                    }
+                }
+            }
+            false => {
+                let mut xs = (min(x1, x2)..=max(x1, x2)).collect::<Vec<_>>();
+                let mut ys = (min(y1, y2)..=max(y1, y2)).collect::<Vec<_>>();
+
+                // Restore original order if necessary
+                if x1 > x2 {
+                    xs.reverse();
+                }
+                if y1 > y2 {
+                    ys.reverse();
+                }
+
+                for (x, y) in xs.iter().zip(ys.iter()) {
+                    *vents.entry((*x, *y)).or_insert(0) += 1;
+                }
+            }
+        }
+    }
+
+    vents.values().filter(|&v| *v > 1).count() as u32
+}
+
+fn parse_input(input: &'static str) -> Vec<((u32, u32), (u32, u32))> {
+    let exp = regex::Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
+
+    exp.captures_iter(input)
+        .map(|caps| {
+            let c = caps
+                .iter()
+                .skip(1)
+                .map(|c| c.unwrap().as_str().parse::<u32>().unwrap())
+                .collect::<Vec<_>>();
+
+            ((c[0], c[1]), (c[2], c[3]))
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -42,5 +86,11 @@ mod tests {
     fn part_1() {
         let r = super::part_1(INPUT);
         assert_eq!(r, 5);
+    }
+
+    #[test]
+    fn part_2() {
+        let r = super::part_2(INPUT);
+        assert_eq!(r, 12);
     }
 }
