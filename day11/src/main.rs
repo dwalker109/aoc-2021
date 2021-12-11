acbc10f713c9c02bf0608cdca31520a0eb76509e
Dan Walker <dwalker109@gmail.com>
2021-12-11 11:56:47 +0000

    Day 11: Dumbo Octopus (2/2)

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, collections::HashMap, fmt::Display};
+use std::{cell::RefCell, collections::HashMap};
 
 static INPUT: &str = include_str!("../input");
 
@@ -6,57 +6,22 @@ fn main() {
     env_logger::init();
 
     println!("Part 1: {}", part_1(INPUT));
+    println!("Part 2: {}", part_2(INPUT));
 }
 
 #[logging_timer::time]
 fn part_1(input: &'static str) -> u32 {
-    let mut cavern = Cavern(
-        input
-            .lines()
-            .enumerate()
-            .flat_map(|(y, l)| {
-                l.chars()
-                    .enumerate()
-                    .map(move |(x, c)| {
-                        (
-                            (x as isize, y as isize),
-                            RefCell::new(c.to_digit(10).unwrap()),
-                        )
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .collect::<HashMap<_, _>>(),
-    );
+    let mut cavern = Cavern::from(input);
 
     let mut flashes = 0;
 
     for _ in 1..=100 {
-        for (_, e) in cavern.0.iter_mut() {
-            *e.borrow_mut() += 1;
-        }
+        cavern.start_step();
 
-        let mut needs_scan = true;
-        while needs_scan {
-            needs_scan = false;
-
-            for ((x, y), e) in cavern
-                .0
-                .iter()
-                .filter(|(_, e)| *(*e).borrow() != 0 && *(*e).borrow() > 9)
-            {
-                for x in x - 1..=x + 1 {
-                    for y in y - 1..=y + 1 {
-                        if let Some(e) = cavern.0.get(&(x, y)) {
-                            if *e.borrow_mut() != 0 {
-                                *e.borrow_mut() += 1;
-                            }
-                        }
-                    }
-                }
-
+        while cavern.rescan_needed() {
+            for octopus in cavern.will_flash() {
+                cavern.flash_neighbours(octopus);
                 flashes += 1;
-                *e.borrow_mut() = 0;
-                needs_scan = true;
             }
         }
     }
@@ -64,21 +29,101 @@ fn part_1(input: &'static str) -> u32 {
     flashes
 }
 
-#[derive(Debug)]
-struct Cavern(HashMap<(isize, isize), RefCell<u32>>);
+#[logging_timer::time]
+fn part_2(input: &'static str) -> u32 {
+    let mut cavern = Cavern::from(input);
 
-impl Display for Cavern {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut out = String::new();
-        for y in 0..10isize {
-            for x in 0..10isize {
-                let c = *self.0.get(&(x, y)).unwrap().borrow();
-                out.push_str(&c.to_string());
+    for step in 1.. {
+        cavern.start_step();
+
+        while cavern.rescan_needed() {
+            for octopus in cavern.will_flash() {
+                cavern.flash_neighbours(octopus);
             }
-            out.push('\n');
+
+            if cavern.was_simultaneous_flash() {
+                return step;
+            }
+        }
+    }
+
+    unreachable!();
+}
+
+struct Cavern {
+    octopi: HashMap<(isize, isize), RefCell<u32>>,
+    settled: RefCell<bool>,
+}
+
+impl From<&'static str> for Cavern {
+    fn from(input: &'static str) -> Cavern {
+        Self {
+            octopi: input
+                .lines()
+                .enumerate()
+                .flat_map(|(y, l)| {
+                    l.chars().enumerate().map(move |(x, c)| {
+                        (
+                            (x as isize, y as isize),
+                            RefCell::new(c.to_digit(10).unwrap()),
+                        )
+                    })
+                })
+                .collect::<HashMap<_, _>>(),
+
+            settled: RefCell::new(true),
+        }
+    }
+}
+
+impl Cavern {
+    fn start_step(&mut self) {
+        for (_, e) in &self.octopi {
+            *e.borrow_mut() += 1;
         }
 
-        write!(f, "{}", out)
+        *self.settled.borrow_mut() = false;
+    }
+
+    fn rescan_needed(&self) -> bool {
+        *self.settled.borrow() == false
+    }
+
+    fn will_flash(&self) -> impl Iterator<Item = (&(isize, isize), &RefCell<u32>)> {
+        let iter = self
+            .octopi
+            .iter()
+            .filter(|(_, energy)| *(energy).borrow() != 0 && *(energy).borrow() > 9);
+
+        if iter.clone().count() == 0 {
+            *self.settled.borrow_mut() = true;
+        } else {
+            *self.settled.borrow_mut() = false;
+        }
+
+        iter
+    }
+
+    fn flash_neighbours(&self, ((s_x, s_y), s_energy): (&(isize, isize), &RefCell<u32>)) {
+        for x in s_x - 1..=s_x + 1 {
+            for y in s_y - 1..=s_y + 1 {
+                if let Some(energy) = self.octopi.get(&(x, y)) {
+                    if *energy.borrow_mut() != 0 {
+                        *energy.borrow_mut() += 1;
+                    }
+                }
+            }
+        }
+
+        *s_energy.borrow_mut() = 0;
+    }
+
+    fn was_simultaneous_flash(&self) -> bool {
+        if self.octopi.iter().all(|(_, e)| *(*e).borrow() == 0) {
+            return true;
+        }
+
+        false
     }
 }
 
@@ -90,5 +135,11 @@ mod tests {
     fn part_1() {
         let r = super::part_1(INPUT);
         assert_eq!(r, 1656)
+    }
+
+    #[test]
+    fn part_2() {
+        let r = super::part_2(INPUT);
+        assert_eq!(r, 195)
     }
 }
